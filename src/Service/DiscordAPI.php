dae701ff3e453bf1fac9183d59ab0f89d00e81e4
Andrew Lehman <aplehm@gmail.com>
2022-09-25 15:09:21 -0400

    Add base class for Discord API classes

---
+++ src/Service/DiscordAPI.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+abstract class DiscordAPI
+{
+    private const BASE_URL = 'https://discord.com/api/v10';
+    private HttpClientInterface $client;
+
+    private function sendRequest(string $url, array $options = [], string $method = 'GET')
+    {
+        $fullUrl = self::BASE_URL . "/$url";
+        $this->client->request($method, $fullUrl, $options);
+    }
+}
