8683caa2d6c502c80b9d005584fcaea0db5c53ee
Andrew Lehman <aplehm@gmail.com>
2022-09-25 17:29:19 -0400

    Add enum for command option type

---
+++ src/Enum/CommandOptionType.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App\Enum;
+
+/**
+ * Represent an Application Command Option Type
+ * https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type
+ */
+enum CommandOptionType: int
+{
+    case SUB_COMMAND = 1;
+    case SUB_COMMAND_GROUP = 2;
+    case STRING = 3;
+    case INTEGER = 4;
+    case BOOLEAN = 5;
+    case USER = 6;
+    case CHANNEL = 7;
+    case ROLE = 8;
+    case MENTIONABLE = 9;
+    case NUMBER = 10;
+    case ATTACHMENT = 11;
+}
