9af821162dc89e82967096168f4b67582c14fa3b
Andrew Lehman <aplehm@gmail.com>
2022-10-16 15:10:48 -0400

    Use new InteractionRequest class

--- src/Controller/InteractionController.php
+++ src/Controller/InteractionController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\DTO\InteractionRequest;
 use App\Enum\InteractionType;
 use App\Service\InteractionAuthorizer;
+use App\Service\InteractionRequestConverter;
 use App\Service\PingPong;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class InteractionController extends AbstractController
 {
     #[Route('/interactions', methods: ['POST'])]
-    public function interactionHandler(InteractionAuthorizer $interactionAuthorizer, PingPong $pingPong,Request $request): Response
+    public function interactionHandler(InteractionAuthorizer $interactionAuthorizer, PingPong $pingPong,Request $request, InteractionRequestConverter $interactionConverter): Response
     {
         $signature = $request->headers->get('X-Signature-Ed25519');
         $timestamp = $request->headers->get('X-Signature-Timestamp');
@@ -23,14 +25,13 @@ class InteractionController extends AbstractController
             return new Response(status: 401);
         }
 
-        // TODO: Create service for parsing interaction data
         $body = json_decode($rawBody, true);
-        $interactionType = InteractionType::from($body['type']);
-        if ($interactionType === InteractionType::PING) {
+        $interaction = $interactionConverter::convert($body);
+        if ($interaction->type === InteractionType::PING) {
             return $pingPong->pong();
         }
 
-        // Actual interaction request
+        // Actual interaction response
         return new Response(
             json_encode(['type' => 4, 'data' => ['content' => 'Hello World!']]),
             200,
